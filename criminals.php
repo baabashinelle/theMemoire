406c2aa7d062717be6cc4e54658641c8ff15bc70
Baaba <baabadampare@gmail.com>
2022-05-01 23:42:37 +0000

    edit article on criminals

--- criminals.php
+++ criminals.php
@@ -78,63 +78,92 @@
         </div>
         <p class="pt-4">
           Everyone has heard their fair share of gory stories, ranging from unfortunate child abusers to
-          school shootings that cause needless suffering. These events evoke disdain, anger and public disapproval as they should. I hasten to say that the telos of this essay does not assert that rapists, criminals and the likes should not be secluded from society. This essay is an investigation into what it means for a person to deserve punishment, the principles that govern our notion of retributive punishment, the coherency of these principles and how we can more ethically go about them.
+          school shootings that cause needless suffering. These events evoke disdain, anger and public
+          disapproval as they should. I hasten to say that the telos of this essay does not assert that rapists,
+          criminals and the likes should not be secluded from society. This essay is an investigation into what
+          it means for a person to deserve punishment, the principles that govern our notion of retributive
+          punishment, the coherency of these principles and how we can more ethically go about them.
         </p>
-        <p>To begin with, I would like to invite you to stand up, grab a glass of water and return to continue this essay. Out of the set of people who are reading this piece, some subset did heed my instructions and others, perhaps you, scorned in apathy. The task for the people that did or did not perform the activity is to attempt to explain why they did or did not do it. Without appealing to circular reasoning, try to give a rationale as to why whatever answer you gave pushed you to do what you did or failed to do. We can keep engaging in this exercise and in the limit, we end at an infinite regress. For example, if you asserted that you didn’t go grab a glass of water because you didn’t feel like it and your response to why you did not feel like it was because you were reclining and felt lazy we can yet ask why you were feeling lazy and the questioning goes on ad infinitum. However, the interesting idea is in the fact that the feeling of laziness, say or the feeling of “not feeling like it” was totally arbitrary conceptually. As Sam Harris puts it, albeit, in different words, you did not have the thought, the thought sprung up and then you represented it in your decision. You were not the author of your decision to not-feel-like it, you were merely an effector of this deterministic causal chain.</p>
+        <p>To begin with, I would like to invite you to stand up, grab a glass of water and return to
+          continue this essay. Out of the set of people who are reading this piece, some subset did heed my
+         instructions and others, perhaps you, scorned in apathy. The task for the people that did or did not
+          perform the activity is to attempt to explain why they did or did not do it. Without appealing to
+        circular reasoning, try to give a rationale as to why whatever answer you gave pushed you to do
+        what you did or failed to do. We can keep engaging in this exercise and in the limit, we end at an
+        infinite regress. For example, if you claimed that you didn’t go grab a glass of water because you
+         didn’t feel like it and your response to why you did not feel like it was because you were reclining
+          and felt lazy we can yet ask why you were feeling lazy and the questioning goes on ad infinitum.
+        However, the interesting idea is in the fact that the feeling of laziness, say or the feeling of “not
+         feeling like it” was totally arbitrary conceptually. As Sam Harris puts it, albeit, in different words,
+          you did not have the thought, the thought sprung up and then you represented it in your decision.
+           You were not the author of your decision to not-feel-like it, you were merely an effector of this
+          deterministic causal chain.</p>
         <p>
-          Why this conceptual thought experiment is important is that it brings to light a very important feature that governs 
-          our everyday notion of what it means to act according to a law one gives oneself. Child molesters in most of our 
-          opinions deserve jail time or to be mercilessly beaten or lynched even because we reason that they refuse to 
-          exercise their ability to choose the good in the midst of what is wrong. Interestingly, implicit in this assertion 
-          of the refusal to choose what is right is the idea that the option of engaging in sexual relations with a minor was 
-          even on the table. For most people, that thought on its own is stomach-wrenching but if you went through the paragraph 
-          prior you noticed that we demonstrated that we are not the authors of our thoughts themselves but merely effectors of them.
-           The first asymmetry worth noticing is that, who decides which individuals have those thoughts spring up in the fountains 
-           of their minds? Suppose you assert that the kind of thoughts one has is in part a function of their environment, then we
-            have yet again bumped our heads on the wall of the arbitrariness of the environment one is fortunate or unfortunate to 
-            have been born in. The next asymmetry will be in that out of the group that does have those thoughts who determines who
-             has enough self-control or willpower to stifle such urges? The answer to that is yet again, the arbitrariness of the lottery 
-             of life. So there appears to be at least an inconsistency in the manner in which view the ”autonomy” we all have. Sam Harris 
-             in his book, Free Will, gives an excellent example that illustrates the inconsistency with which we weigh individual autonomy. 
-             It is expressed (reconstructed) in the scenarios below.
+          Why this conceptual thought experiment is important is that it brings to light a very important
+            feature that governs our everyday notion of what it means to act according to a law one gives
+oneself. Child molesters in most of our opinions deserve jail time or to be mercilessly beaten or
+lynched even because we reason that they refuse to exercise their ability to choose the good in the
+midst of what is wrong. Interestingly, implicit in this assertion of the refusal to choose what is right
+is the idea that the option of engaging in sexual relations with a minor was even on the table. For
+most people, that thought on its own is stomach-wrenching but if you went through the paragraph
+prior you noticed that we demonstrated that we are not the authors of our thoughts themselves but
+merely effectors of them. The first asymmetry worth noticing is that, who decides which individuals
+have those thoughts spring up in the fountains of their minds? Suppose you state that the kind of
+thoughts one has is in part a function of their environment, then we have yet again bumped our
+heads on the wall of the arbitrariness of the environment one is fortunate or unfortunate to have
+been born in. The next asymmetry will be in that out of the group that does have those thoughts who
+determines who has enough self-control or willpower to stifle such urges? The answer to that is yet
+again, the arbitrariness of the lottery of life. So there appears to be at least an inconsistency in the
+manner in which view the ”autonomy” we all have. Sam Harris in his book, Free Will, gives an
+excellent example that illustrates the inconsistency with which we weigh individual autonomy. It is
+expressed (reconstructed) in the scenarios below.
              <ul>
                  <li>A young boy who was subjected to physical and emotional abuse takes his dad’s revolver and shoots three of his colleagues.</li>
                  <li>An older man who also had a history of physical and emotional abuse uses his revolver to shoot three of his workmates.</li>
                  <li>An older man who had a brain tumour affecting his pre-frontal cortex (which helps to inhibit behaviour (Dillon and Pizzagalli, 2007)) shoots three of his family members.</li>
              </ul>
              <p>
-                In many cases, we view scenarios 1 and 3 as individuals that are victims of circumstance. For scenario 3 we may assert that 
-             the older man was a victim of his biology and hence it might not be morally sound to hold them as accountable as a person 
-             who in our view, chooses to engage in this abhorrent act. Granted that not many criminals have tumours creeping over their 
-             pre-frontal cortex, we can still make the argument that the older man in scenario 2 is a victim of a combination of his environment 
-             and biology. The former because it is obvious that our external environment has a great impact on the way we see the world and 
-             the latter because it is random who has the biological ability to stifle such desires. So the argument here is that there is no 
-             conceptual difference between the individual who is supposedly a victim of his biology and the older individual who pulled the 
-             trigger on his three workmates.
+                In many cases, we view scenarios 1 and 3 as individuals that are victims of circumstance. For
+scenario 3 we may contend that the older man was a victim of his biology and hence it might not be
+morally sound to hold them as accountable as a person who in our view, chooses to engage in this
+abhorrent act. Granted that not many criminals have tumours creeping over their pre-frontal cortex,
+we can still make the argument that the older man in scenario 2 is a victim of a combination of his
+environment and biology. The former because it is obvious that our external environment has a
+great impact on the way we see the world and the latter because it is random who has the biological
+ability to stifle such desires. So the argument here is that there is no conceptual difference between
+the individual who is supposedly a victim of his biology and the older individual who pulled the
+trigger on his three workmates. 
              </p>
         </p>
         <p>
-          Consequentially, you might be wondering if this worldview shakes our justice system’s 
-          foundation and you will be right because such a view does in fact question the moral grounds 
-          of retributive justice. If thoughts are picked by sentient creatures from an abstract realm 
-          of mental and biological processes that we are of no direct control of, then the moral accountancy 
-          that the current justice system calls for is overpricing and may even be immoral. This doesn’t mean 
-          that we must resort to chaotic virtueless mayhem or fatalistic tendencies. It is a call to look at 
-          the perpetrators of “evil” acts with much more empathy than retributive justice currently provides. 
-          This understanding does not trash the utility of locking dangerous people up. As Harris puts it, it is
-           perfectly coherent to agree that those who commit atrocious crimes are not themselves to be blamed and 
-           still recognise that they must be secluded from society for a prosperous civilisation to ensue.
+          Consequentially, you might be wondering if this worldview shakes our justice system’s
+foundation and you will be right because such a view does in fact question the moral grounds of
+retributive justice. If thoughts are picked by sentient creatures from an abstract realm of mental and
+biological processes that we are of no direct control of, then the moral accountancy that the current
+justice system calls for is overpricing and may even be immoral. This doesn’t mean that we must
+resort to chaotic virtueless mayhem or fatalistic tendencies. It is a call to look at the perpetrators of
+“evil” acts with much more empathy than retributive justice currently provides. This understanding
+does not trash the utility of locking dangerous people up. As Harris puts it, it is perfectly coherent to
+agree that those who commit atrocious crimes are not themselves to be blamed and still recognise
+that they must be secluded from society for a prosperous civilisation to ensue. The caveat here is
+that if there are rehabilitative programs to renew the minds of criminals and rapists we must do that
+over just seclusion. You will also notice that under the idea that people deserve punishment, it
+would be more apt to keep them in prison cells than it will be to give them a cure. That is the danger
+of this type of thinking. Nonetheless, I must note that this is far more complicated than I present,
+because it might be the case that even after a cure has been given to these criminals, the distress
+they bring to victims (by freely existing in society) still warrants their seclusion. 
         </p>
         <p>
-          However, the important change of mentality is the switch from the notion of punishment 
-          deservingness to a more empathetic view of the victims of circumstance. In Ghana, many criminals 
-          are subjected to torturous and inhumane treatments. Even for the most heinous crimes, I argue that 
-          it is in no way acceptable for a creature as sentient as a human to be subjected to such horror. To push 
-          back by saying that the people who enact these torturous acts are effectors of their impulse is to say 
-          that such individuals lack the ability to empathise even when flaws in their rationale for doing so are 
-          presented. This ultimately lines up with the imperative with which we asserted makes imprisoning criminals 
-          coherent- It is, do we want to allow individuals who are impermeable to reason and controlled by violent impulses
-           to be free to cause unnecessary suffering in a civilised society?
+          However, the important change of mentality is the switch from the notion of punishment
+deservingness to a more empathetic view of the victims of circumstance. In Ghana, many criminals
+are subjected to torturous and inhumane treatments. Even for the most heinous crimes, I argue that
+it is in no way acceptable for a creature as sentient as a human to be subjected to such horror. To
+push back by saying that the people who torture criminals are effectors of their impulse is to say
+that such individuals (the torturers) lack the ability to empathise even when flaws in their rationale
+for doing so are presented. This ultimately lines up with the imperative with which we asserted
+makes imprisoning criminals coherent- It is, do we want to allow individuals who are impermeable
+to reason and controlled by violent impulses to be free to cause unnecessary suffering in a civilised
+society?
         </p>
         <p>Mr. Larry Asiwome Afun</p>
         <p>University of Toronto</p>
