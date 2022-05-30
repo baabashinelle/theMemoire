b97e5cda8ecfd1a878224d70b095117bbf05ed0e
Baaba <baabadampare@gmail.com>
2022-05-30 22:50:19 +0000

    add article on grace

---
+++ grace.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Written By Larry - The Philosophical Problem of Grace</title>
+    <!--Favicon-->
+    <link
+      rel="shortcut icon"
+      href="assets/images/My project (1).png"
+      type="image/x-icon"
+    />
+
+    <!--Core CSS-->
+    <link rel="stylesheet" href="style.css" />
+
+    <!--Bootstrap CSS-->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+
+    <!--Euclid font-->
+    <link href="fontseuclid/stylesheet.css" rel="stylesheet" type="text/css" />
+
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="fontawesome/css/all.css" />
+
+    <!--Animate on scroll-->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand logo" href="index.php"
+          ><img
+            src="assets/images/My project (1).png"
+              alt="The Memoire"
+              width="80"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto navitems">
+            <a class="nav-link" href="index.php">Home</a>
+            <a class="nav-link" href="index.php">Articles</a>
+            <a class="nav-link" href="about.php">About</a>
+            <a class="nav-link" href="#footer">Subscribe</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="nima-sec">
+      <div class="container">
+        <h3 class="text-center pb-4">
+          The Philosophical Problem of Grace
+        </h3>
+        <div class="text-center">
+          <img
+            src="assets/images/jail.jpg"
+            class="nimasec-pic"
+            width="1000"
+            alt="Jail"
+          />
+        </div>
+        <p class="pt-4">
+          Imagine for an instance that you upload a video on YouTube and become an overnight star or
+you make a tweet and that garners revenue generating attention. Fine-tuning these scenarios within
+the frame of moral saliency, they can be described as instances of divine providence or grace.
+According to a Guite and Soreng in their journal published in Research Gate, “Grace, in the
+Christian understanding, is the unconditional love, the free and undeserved favour of God” (1). This
+essay is a deep dive into the philosophy of this divine providence, whether it is identifiable
+intellectually and if it can only be determined upon hindsight
+        </p>
+        <p>
+          There is a heuristic, I believe, to which most people account for divine providence within their
+lives. I categorise this heuristic in the syllogism below;
+             <ul>
+                 <li>Premise 1 : Nothing that brings good pleasure is wrong</li>
+                 <li>Premise 2: Only God can enforce/permit good pleasure.</li>
+                 <li>Conclusion: All good pleasures are from God only.</li>
+             </ul>
+             <p>
+                In this tripartite, I am defining good pleasure to be any act that minimises suffering thereby
+positively influencing well-being while being either being morally good or morally neutral. This
+syllogism may appear outlandish on the surface, perhaps, due to its formal presentation, but I assert
+that it is a mental model that most Christians (or any other religious folks for that matter) make in
+assessing consequential issues. The illustrations in the introductory paragraph allude to that. The
+claim above relies on the fact that all good things are from God alone and so it follows that all good
+things that occur in one’s life are as a result of divine providence. We can call this divine providence
+based on the axiom that God owes no one (otherwise, it wouldn’t be unmerited favour i.e. grace).
+Now we could be pedantic and inquire as to whether “small pleasures” are products of grace. For
+example, “small pleasures” like making a 3-pointer at a high school basketball game or wining a
+game of frisbee with your friends on a warm Saturday afternoon. Instances like these generally can
+only be labelled as graceful acts insofar as they influence well-being significantly. So we will
+largely think of wining a frisbee match as an act of grace if for instance, it results in one joining the
+national frisbee olympics team, thereby improving the quality of one’s life. The overarching
+conjecture (the syllogism) is a popular line of reasoning whose conclusion holds true so long as it
+entails the premises.
+             </p>
+        </p>
+        <p>
+            The first criticism to this line of thinking, I believe, is best seen through the multiverse
+analogy below. Suppose a group of boys robbed a bank and the following ensued;
+          <ul>
+                 <li>1. In the first world, the boys were caught and in the course of time they changed and became
+respectable people (who led a righteous life).</li>
+                 <li>2. In the second world, the boys were caught but still turned out to be truants, living an unrighteous
+life.</li>
+                 <li>3. In the third world, the boys escaped and in the course of time, upon reflection of their life,
+changed to became people who led righteous lives.</li>
+<li>4. In the fourth world the boys escaped and turned out to be truants all the same.</li>
+             </ul>
+        </p>
+        <p>
+          In the thought experiments, it may seem inconsistent with the syllogism in paragraph one to
+claim that any of the situations was an act of grace simply because it ended in a net positive good. If
+scenario 2 came into fruition (i.e. where the boys were caught and still turned out to be truants), it
+may presuppose that God destined the boys to a life unworthy to be lived since it resorts in
+condemnation. On the other hand, the tripartite will prevent one from labelling scenario 3 as an act
+of divine providence. This is because the action of robbing a bank itself is not moral. It is not a good
+pleasure and so from that line of reasoning, it cannot be from God, even if it results in a net good.
+You can argue as per the illustration 3, that the mere retrospective realisation of one’s wrong leading
+to righteousness is an act of grace. However, that commits you to the view that there can be divine
+conversion of minds, in which case you would have to square that view up with the notion of free
+will that most religious folks preach.
+        </p>
+        <p>A potential source of contention can be found in the 2nd premise. This may be brought out by
+people who promulgate the view that good goods can be planted by the devil to cause or expedite
+net suffering (not necessarily physical suffering). In that world view, getting a job that improves the
+quality of your life (a good pleasure), can be on its own, a ploy by an evil agent. This may be a
+grand ploy to sway an individual from the shadow of the divine supervisor. Since premise 2 states
+that only God can enforce or permit good pleasures, the example, if valid, annuls the whole
+argument because it would mean that good can be permitted or enforced by something other than
+God. This inadvertently becomes a sledge hammer to the proposition is because the conclusion can
+obviously not be true if it does not entail the premise. </p>
+<p>Nonetheless, a critique can also be that deeds themselves are neither evil nor good and that the
+potency for an action or a thing to be good or evil rests on the manner in which a conscious moral
+agent wields them. Therefore, a thing or an opportunity can be graciously given, however, if the
+means by which it is implemented is evil then it can in no way be attributed to a divine hand. This
+results in grace becoming a concept that can only be retrospectively defined. To elaborate, it means
+that no good or service can defined as an act of grace until it is utilised. This case in general is
+permissible if and only if the assertion that only God can grant good pleasures is granted (which has
+a lot of evidence to show). </p>
+<p>In a nutshell, this essay represents an original inquiry into the nuance of divine providence,
+investigating possible arguments that could be made whilst leaving the topic open to further
+discussion. I hope to have done at least one of two things in this essay. To demonstrate that (1) The
+concept of grace is not nearly as intellectually straightforward as we once thought and (2) There are
+intricacies in the conclusions that could be drawn by assigning a deed to be an act of grace.</p>
+        <p>Mr. Larry Asiwome Afun</p>
+        <p>University of Toronto</p>
+        <h2 class="text-center pt-4">Works Cited</h2>
+        <p>
+          Guite, Thangbiakching, and Eric Soreng. “Grace of God: A Phenomenological Inquiry.”
+        </p>
+        <p>
+          ResearchGate, unknown, 3 Dec. 2019, <a href="www.researchgate.net/publication/">www.researchgate.net/publication/</a>
+        </p>
+        <p>337706835_Grace_of_God_A_Phenomenological_Inquiry. Accessed 30 May 2022.</p>
+      </div>
+    </section>
+
+    <!--Footer-->
+    <section class="footer-sec" id="footer">
+      <div class="container">
+        <div class="text-center">
+          <h3>Subscribe to receive latest updates!</h3>
+          <hr
+          width="300"
+          style="margin: auto"
+        />
+          <p class="pt-4">
+            In order not to miss out on my latest articles, subscribe to The Memoire now.
+          </p>
+          <!--<div class="input-group mb-3 pt-4">
+            <input
+              type="text"
+              class="form-control "
+              placeholder="Email Address"
+              required
+              aria-describedby="button-addon2"
+              style="width: 70px;"
+            />
+            <button
+              class=" btn-outline-secondary"
+              type="submit"
+              id="button-addon2"
+            >
+              Subscribe <i class="fas fa-paper-plane"></i>
+            </button>
+          </div>-->
+          <form action="index.php" method="POST">
+            <?php
+            $userEmail = ""; //first we leave this field blank
+            if(isset($_POST['subscribe'])){ //if subscribe button clicked
+               $userEmail = $_POST['email']; //getting user email
+               if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //validating user entered email
+                 $subject = "Thanks for subscribing!";
+                 $message = "Thanks for subscribing to The Memoire. You'll receive notifications on latest updates anytime I post!";
+                 $sender = "From: thememoirebylarry@gmail.com";
+                 if(mail($userEmail, $subject, $message, $sender )){ //php function to send mail
+                    ?>
+                    <!--Successful-->
+                 <div class="alert"> Thanks for Subscribing!</div>
+                 <?php
+                 $userEmail = "";
+                 }else{
+                     ?>
+                     <!-- Not Successful-->
+                 <div class="alert"> Failed to send email.</div>
+                 <?php
+                 }
+               }else{
+                 ?>
+                 <div class="alert"> <?php echo $userEmail?> is not valid.</div>
+                 <?php
+               }
+            }
+            ?>
+            <div class="input-group mb-3 pt-4">
+            <input
+              type="text"
+              class="form-control "
+              placeholder="Email Address" required
+              name="email"
+              value="<?php echo $userEmail ?>"
+              style="width: 70px;"
+            />
+            <button
+              class=" btn-outline-secondary"
+              type="submit"
+              name="subscribe"
+              value="subscribe"
+            >
+              Subscribe <i class="fas fa-paper-plane"></i>
+            </button>
+          </div>
+          </form>
+          <p>© The Memoire, 2022</p>
+        </div>
+      </div>
+    </section>
+    <!--For Bootstrap-->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+
+    <!--AOS-->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+  </body>
+</html>
